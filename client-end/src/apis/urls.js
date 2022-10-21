6936d522b0699cd850c9a6a6d4bd80851a8d8571
TheChiran <tonmoychiran449@gmail.com>
2022-10-21 21:33:01 +0600

    [Task-> 1. changed cgpa validation range from 5 to 4]

--- client-end/src/apis/urls.js
+++ client-end/src/apis/urls.js
@@ -1,2 +1,2 @@
-export const dev_URL = "http://localhost:8000/api/v1";
-// export const dev_URL = "https://v2-technology-assessment.onrender.com/api/v1";
+// export const dev_URL = "http://localhost:8000/api/v1";
+export const dev_URL = "https://v2-technology-assessment.onrender.com/api/v1";
